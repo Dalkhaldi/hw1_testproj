9eb93b99330aa96bbc784c4f11eb08c4bc912725
jcc <jcc@DESKTOP-8C0JC9G>
2019-02-20 23:43:08 +0200

    commit1

---
+++ src/hw1_testproj/main.java
@@ -0,0 +1,30 @@
+package hw1_testproj;
+ 
+public class main
+{    
+	int valu;
+	int nprod;
+	 
+	public main()
+	 
+	 {
+		 valu = 0;
+		 nprod = 0;
+	 }
+	
+
+	public int cartval()
+	{
+		 return valu;
+	}
+	public void insertbook(int bookn,int val)
+	{
+		valu  = valu+val;
+		nprod = nprod+bookn;
+	}
+	public int cartbook()
+	{
+		return nprod;
+	}
+
+}
